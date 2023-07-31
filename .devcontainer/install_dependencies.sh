a2119122e45a91dda299e928649a70214f923981
Kevin Knights <kevinknights29@hotmail.com>
2023-07-31 00:10:02 -0500

    added scrip to install deps

---
+++ .devcontainer/install_dependencies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if the path to the requirements file was provided
+if [ -z "$1" ]; then
+    echo "Please provide the path to the requirements file"
+    exit 1
+fi
+
+# Installing prerequisites
+apt-get update && \
+    apt-get install -y \
+    build-essential \
+    python3-launchpadlib \
+    python3-dev \
+    python3-pip \
+    python3-venv \
+    gcc \
+    && apt update
+
+# Initialize the virtual environment
+python3 -m venv .venv
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Set the path of the requirements.txt file
+REQUIREMENTS_FILE=$1
+
+# Set llama.cpp environment variables
+export CMAKE_ARGS="-DLLAMA_BLAS=ON -DLLAMA_BLAS_VENDOR=OpenBLAS"
+export FORCE_CMAKE=1
+
+# Install the Python packages
+pip3 install -r $REQUIREMENTS_FILE
