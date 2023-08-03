a2e9060c6df62ab783aacd5bc4787b37e537c181
Kevin Knights <kevinknights29@hotmail.com>
2023-08-02 23:19:46 -0500

    added cuda toolkit install script

---
+++ .devcontainer/install_cuda_toolkit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Source: https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=WSL-Ubuntu&target_version=2.0&target_type=deb_local
+
+# To check your OS architecture, run:
+# lscpu
+
+# Dependency
+apt install -y wget
+
+# Install CUDA Toolkit
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.2.1/local_installers/cuda-repo-wsl-ubuntu-12-2-local_12.2.1-1_amd64.deb
+dpkg -i cuda-repo-wsl-ubuntu-12-2-local_12.2.1-1_amd64.deb
+cp /var/cuda-repo-wsl-ubuntu-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
+apt update
+apt install -y cuda
