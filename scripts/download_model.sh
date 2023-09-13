9bcf7f7b0448b85e350e6bfefceb1a62376ccff4
Kevin Knights <kevinknights29@hotmail.com>
2023-09-12 22:12:48 -0500

    updated model link to download GGUF models

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -6,10 +6,5 @@ cd /opt/models
 
 # 4 bit quantized model
 if [ ! -f llama-2-7b-chat.ggmlv3.q4_0.bin ]; then
-    wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q4_0.bin
+    wget https://huggingface.co/TheBloke/Llama-2-7b-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_0.gguf
 fi
-
-# # 8 bit quantized model
-# if [ ! -f llama-2-7b-chat.ggmlv3.q8_0.bin ]; then
-#     wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q8_0.bin
-# fi
